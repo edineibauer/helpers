889296f6518b10a67a285cdb676b252fcb371e55
Edinei <galera.org@gmail.com>
2018-01-11 10:14:27 -0200

    folder template up

--- src/Helpers/Template.php
+++ src/Helpers/Template.php
@@ -89,7 +89,7 @@ class Template
         //        $this->smart->caching = true;
         //        $this->smart->cache_lifetime = 120;
 
-        $this->smart->setTemplateDir($this->folder ? (!preg_match('/:/i', $this->folder) ? $_SERVER['DOCUMENT_ROOT'] . "/" : "") . $this->folder : "vendor/conn/{$this->library}/tpl");
+        $this->smart->setTemplateDir($this->folder ?? "vendor/conn/{$this->library}/tpl");
     }
 
     private function preData()
