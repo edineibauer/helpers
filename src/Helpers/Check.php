ae2f0370e5046584443579e9c0df7f9879912388
Edinei <galera.org@gmail.com>
2018-01-20 17:28:43 -0200

    validação de CNPJ

--- src/Helpers/Check.php
+++ src/Helpers/Check.php
@@ -88,14 +88,48 @@ class Check
         return str_replace('?', '-', utf8_decode(strtolower(utf8_encode(self::$Data))));
     }
 
-    public static function cnpj($cnpj = false)
+    /**
+     * Valida CNPJ
+     *
+     * @param string $cnpj
+     * @return bool
+    */
+    public static function cnpj(string $cnpj) :bool
     {
+        $cnpj = preg_replace('/[^0-9]/', '', (string) $cnpj);
 
+        // Valida tamanho
+        if (strlen($cnpj) != 14)
+            return false;
+
+        // Valida primeiro dígito verificador
+        for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+        if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+            return false;
+
+        // Valida segundo dígito verificador
+        for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+        return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
     }
 
-    public static function cpf($cpf = false)
+    /**
+     * Valida CPF
+     *
+     * @param string $cpf
+     * @return bool
+     */
+    public static function cpf(string $cpf) :bool
     {
-
         $cpf = preg_replace('/[^0-9]/', '', (string)$cpf);
         if (strlen($cpf) !== 11 || $cpf === '00000000000' || $cpf === '11111111111' || $cpf === '22222222222' || $cpf === '33333333333' || $cpf === '44444444444' || $cpf === '55555555555' || $cpf === '66666666666' || $cpf === '77777777777' || $cpf === '88888888888' || $cpf === '99999999999'):
             return false;
