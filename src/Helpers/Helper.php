af734babe6abe0183e9de945c7a4ef80fc32f8eb
Edinei <galera.org@gmail.com>
2018-04-09 14:30:18 -0300

    hex to rgb

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -33,6 +33,28 @@ class Helper
     }
 
     /**
+     * Converte cor hexadecimal para RGB ->retorna array('red' => 255, 'green' => 112, 'blue' => 114)
+     * @param string $colour
+     * @return mixed
+     */
+    function hex2rgb( string $colour ) {
+        if ( $colour[0] == '#' )
+            $colour = substr( $colour, 1 );
+
+        if ( strlen( $colour ) == 6 )
+            list( $r, $g, $b ) = array( $colour[0] . $colour[1], $colour[2] . $colour[3], $colour[4] . $colour[5] );
+        elseif ( strlen( $colour ) == 3 )
+            list( $r, $g, $b ) = array( $colour[0] . $colour[0], $colour[1] . $colour[1], $colour[2] . $colour[2] );
+        else
+            return false;
+
+        $r = hexdec( $r );
+        $g = hexdec( $g );
+        $b = hexdec( $b );
+        return array( 'red' => $r, 'green' => $g, 'blue' => $b );
+    }
+
+    /**
      * Converte os valores no tipo string de um array nos tipos corretos dos valores
      * ex: convert "true" => true; "12" => 12
      *
